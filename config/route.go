aa19e1cb07e0957063cbe47e40c5f566e67f6530
Wenbo Han <hwbrzzl@gmail.com>
2023-02-17 23:06:44 +0800

    Add configuration (#34)

---
+++ config/route.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/goravel/framework/facades"
+)
+
+func init() {
+	config := facades.Config
+	config.Add("route", map[string]any{
+		// HTTP Host
+		"host": config.Env("APP_HOST", "127.0.0.1:3000"),
+		// HTTPS Configuration
+		"tls": map[string]any{
+			// HTTPS Host
+			"host": config.Env("APP_HOST", "127.0.0.1:3000"),
+			// SSL Certificate
+			"ssl": map[string]any{
+				// ca.pem
+				"cert": "",
+				// ca.key
+				"key": "",
+			},
+		},
+	})
+}
