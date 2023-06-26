fff952e6aa7c8680feffb31a7ac86e2175965735
kkumar-gcc <krishkumar9352@gmail.com>
2023-06-26 16:00:47 +0530

    Feat: Add Seeding functionality

---
+++ app/providers/database_service_provider.go
@@ -0,0 +1,22 @@
+package providers
+
+import (
+	"goravel/database/seeders"
+
+	"github.com/goravel/framework/contracts/database/seeder"
+	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/facades"
+)
+
+type DatabaseServiceProvider struct {
+}
+
+func (receiver *DatabaseServiceProvider) Register(app foundation.Application) {
+	facades.Seeder().Register([]seeder.Seeder{
+		&seeders.DatabaseSeeder{},
+	})
+}
+
+func (receiver *DatabaseServiceProvider) Boot(app foundation.Application) {
+
+}
