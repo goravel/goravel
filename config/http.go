0eb8de260f6272b3a1b08afaf31fad085dbdd8c9
耗子 <i@haozi.net>
2023-11-21 11:59:24 +0800

    feat: use KB

--- config/http.go
+++ config/http.go
@@ -14,8 +14,8 @@ func init() {
 		// HTTP Drivers
 		"drivers": map[string]any{
 			"gin": map[string]any{
-				// Optional, default is 4MB
-				"body_limit": 4,
+				// Optional, default is 4096 KB
+				"body_limit": 4096,
 				"route": func() (route.Route, error) {
 					return ginfacades.Route("gin"), nil
 				},
