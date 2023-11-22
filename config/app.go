ba806848d150b072833a8f44cba3d3c45f110a11
kkumar-gcc <krishkumar9352@gmail.com>
2023-11-22 14:39:46 +0530

    add configuration for localization feature

--- config/app.go
+++ config/app.go
@@ -56,6 +56,20 @@ func init() {
 		// More: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 		"timezone": carbon.UTC,
 
+		// Application Locale Configuration
+		//
+		// The application locale determines the default locale that will be used
+		// by the translation service provider.You are free to set this value
+		// to any of the locales which will be supported by the application.
+		"locale": "en",
+
+		// Application Fallback Locale
+		//
+		// The fallback locale determines the locale to use when the current one
+		// is not available.You may change the value to correspond to any of
+		// the language folders that are provided through your application.
+		"fallback_locale": "en",
+
 		// Encryption Key
 		//
 		// 32 character string, otherwise these encrypted strings
