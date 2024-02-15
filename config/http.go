5246c5221cd0f6f6512e630991c51574add08ff4
耗子 <i@haozi.net>
2024-02-15 15:55:58 +0800

    feat: add header_limit config

--- config/http.go
+++ config/http.go
@@ -14,11 +14,12 @@ func init() {
 		// HTTP Drivers
 		"drivers": map[string]any{
 			"gin": map[string]any{
+				// Optional, default is 4096 KB
+				"body_limit": 4096,
+				"header_limit": 4096,
 				"route": func() (route.Route, error) {
 					return ginfacades.Route("gin"), nil
 				},
-				// Optional, default is 4096 KB
-				"body_limit": 4096,
 			},
 		},
 		// HTTP URL
