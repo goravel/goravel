5fcd5d52dfb56d74eb71df2f3096baa3224fa6b6
kkumar-gcc <krishkumar9352@gmail.com>
2024-03-30 09:55:16 +0530

    add more session configuration

--- config/session.go
+++ config/session.go
@@ -25,6 +25,8 @@ func init() {
 		// to immediately expire on the browser closing, set that option.
 		"lifetime": config.Env("SESSION_LIFETIME", 120),
 
+		"expire_on_close": config.Env("SESSION_EXPIRE_ON_CLOSE", false),
+
 		// Session File Location
 		//
 		// When using the file session driver, we need a location where the
@@ -32,11 +34,52 @@ func init() {
 		// different location may be specified. This is only needed for file sessions.
 		"files": path.Storage("framework/sessions"),
 
+		// Session Sweeping Lottery
+		//
+		// Some session drivers must manually sweep their storage location to get
+		// rid of old sessions from storage. Here are the chances out of 100 that
+		// the sweeper will sweep the storage location. The default is 2 out of 100.
+		"lottery": []int{2, 100},
+
 		// Session Cookie Name
 		//
 		// Here you may change the name of the cookie used to identify a session
 		// in the application. The name specified here will get used every time
 		// a new session cookie is created by the framework for every driver.
 		"cookie": config.Env("SESSION_COOKIE", str.Of(config.GetString("app.name")).Snake().Lower().String()+"_session"),
+
+		// Session Cookie Path
+		//
+		// The session cookie path determines the path for which the cookie will
+		// be regarded as available.Typically, this will be the root path of
+		// your application, but you are free to change this when necessary.
+		"path": config.Env("SESSION_PATH", "/"),
+
+		// Session Cookie Domain
+		//
+		// Here you may change the domain of the cookie used to identify a session
+		// in your application.This will determine which domains the cookie is
+		// available to in your application.A sensible default has been set.
+		"domain": config.Env("SESSION_DOMAIN", ""),
+
+		// HTTPS Only Cookies
+		//
+		// By setting this option to true, session cookies will only be sent back
+		// to the server if the browser has an HTTPS connection. This will keep
+		// the cookie from being sent to you if it cannot be done securely.
+		"secure": config.Env("SESSION_SECURE", false),
+
+		// HTTP Access Only
+		//
+		// Setting this to true will prevent JavaScript from accessing the value of
+		// the cookie, and the cookie will only be accessible through the HTTP protocol.
+		"http_only": config.Env("SESSION_HTTP_ONLY", true),
+
+		// Same-Site Cookies
+		//
+		// This option determines how your cookies behave when cross-site requests
+		// take place, and can be used to mitigate CSRF attacks.By default, we
+		// will set this value to "lax" since this is a secure default value.
+		"same_site": config.Env("SESSION_SAME_SITE", "lax"),
 	})
 }
