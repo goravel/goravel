b479cc72220d14477db183387d9a1463f7eb4737
kkumar-gcc <krishkumar9352@gmail.com>
2024-04-01 13:35:32 +0530

    update session configuration

--- config/session.go
+++ config/session.go
@@ -22,7 +22,8 @@ func init() {
 		//
 		// Here you may specify the number of minutes that you wish the session
 		// to be allowed to remain idle before it expires. If you want them
-		// to immediately expire on the browser closing, set that option.
+		// to immediately expire when the browser is closed, then you may
+		// indicate that via the expire_on_close configuration option.
 		"lifetime": config.Env("SESSION_LIFETIME", 120),
 
 		"expire_on_close": config.Env("SESSION_EXPIRE_ON_CLOSE", false),
