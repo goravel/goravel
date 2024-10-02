483aed067be65eb316d5f931e3114061c3e38365
Bowen <hwbrzzl@gmail.com>
2024-10-02 22:47:01 +0800

    update migration

--- database/migrations/20240915060148_create_users_table.go
+++ database/migrations/20240915060148_create_users_table.go
@@ -13,17 +13,13 @@ func (r *M20240915060148CreateUsersTable) Signature() string {
 	return "20240915060148_create_users_table"
 }
 
-// Connection The database connection that should be used by the migration.
-func (r *M20240915060148CreateUsersTable) Connection() string {
-	return ""
-}
-
 // Up Run the migrations.
 func (r *M20240915060148CreateUsersTable) Up() {
 	facades.Schema().Create("users", func(table migration.Blueprint) {
 		table.ID("id")
-		table.String("migration")
-		table.Integer("batch")
+		table.String("name")
+		table.String("email")
+		table.String("password")
 	})
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"github.com/goravel/framework/contracts/database/migration"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/goravel/framework/facades"
 
 	"goravel/bootstrap"
@@ -11,32 +14,26 @@ func main() {
 	// This bootstraps the framework and gets it ready for use.
 	bootstrap.Boot()
 
-	//// Create a channel to listen for OS signals
-	//quit := make(chan os.Signal)
-	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	//
-	//// Start http server by facades.Route().
-	//go func() {
-	//	if err := facades.Route().Run(); err != nil {
-	//		facades.Log().Errorf("Route Run error: %v", err)
-	//	}
-	//}()
-	//
-	//// Listen for the OS signal
-	//go func() {
-	//	<-quit
-	//	if err := facades.Route().Shutdown(); err != nil {
-	//		facades.Log().Errorf("Route Shutdown error: %v", err)
-	//	}
-	//
-	//	os.Exit(0)
-	//}()
-	//
-	//select {}
+	// Create a channel to listen for OS signals
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-	facades.Schema().Create("users", func(table migration.Blueprint) {
-		table.ID("id")
-		table.String("migration")
-		table.Integer("batch")
-	})
+	// Start http server by facades.Route().
+	go func() {
+		if err := facades.Route().Run(); err != nil {
+			facades.Log().Errorf("Route Run error: %v", err)
+		}
+	}()
+
+	// Listen for the OS signal
+	go func() {
+		<-quit
+		if err := facades.Route().Shutdown(); err != nil {
+			facades.Log().Errorf("Route Shutdown error: %v", err)
+		}
+
+		os.Exit(0)
+	}()
+
+	select {}
 }
