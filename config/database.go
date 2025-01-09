d5b895596c3de89245571a0008840291d26b85f2
Wenbo Han <hwbrzzl@gmail.com>
2025-01-09 08:36:11 +0800

    chore: [#539] Remove the SQL migrator module (#561)

--- config/database.go
+++ config/database.go
@@ -98,10 +98,8 @@ func init() {
 		// This table keeps track of all the migrations that have already run for
 		// your application. Using this information, we can determine which of
 		// the migrations on disk haven't actually been run in the database.
-		// Available Drivers: "default", "sql"
 		"migrations": map[string]any{
-			"driver": "default",
-			"table":  "migrations",
+			"table": "migrations",
 		},
 
 		// Redis Databases
