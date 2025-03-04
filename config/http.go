98da35ef4469f8eaf4180c82f0c1e1aa1f14513f
kkumar-gcc <krishkumar9352@gmail.com>
2025-03-04 08:43:18 +0530

    move client config

--- config/http.go
+++ config/http.go
@@ -53,11 +53,11 @@ func init() {
 		// HTTP Client Configuration
 		"client": map[string]any{
 			"base_url":                config.GetString("HTTP_CLIENT_BASE_URL"),
-			"timeout":                 config.GetInt("HTTP_CLIENT_TIMEOUT"),
+			"timeout":                 config.GetDuration("HTTP_CLIENT_TIMEOUT"),
 			"max_idle_conns":          config.GetInt("HTTP_CLIENT_MAX_IDLE_CONNS"),
 			"max_idle_conns_per_host": config.GetInt("HTTP_CLIENT_MAX_IDLE_CONNS_PER_HOST"),
 			"max_conns_per_host":      config.GetInt("HTTP_CLIENT_MAX_CONN_PER_HOST"),
-			"idle_conn_timeout":       config.GetInt("HTTP_CLIENT_IDLE_CONN_TIMEOUT"),
+			"idle_conn_timeout":       config.GetDuration("HTTP_CLIENT_IDLE_CONN_TIMEOUT"),
 		},
 	})
 }
