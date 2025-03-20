776fee0c9ae6d7ff7f1f0842a4184a2ac963c9b3
Mohan Raj <praem1990@gmail.com>
2025-03-20 14:20:26 +0530

    Update config/auth.go for the new custom auth driver feature (#638)
    * Update config/auth.go for the new custom auth driver feature
    
    * Add a supported comment

--- config/auth.go
+++ config/auth.go
@@ -29,7 +29,15 @@ func init() {
 		// Supported: "jwt"
 		"guards": map[string]any{
 			"user": map[string]any{
-				"driver": "jwt",
+				"driver":   "jwt",
+				"provider": "user",
+			},
+		},
+
+		// Supported: "orm"
+		"providers": map[string]any{
+			"user": map[string]any{
+				"driver": "orm",
 			},
 		},
 	})
