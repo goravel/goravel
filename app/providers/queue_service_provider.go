605ebb0329cfe8527cdaed743682cb3209ee1dc9
almas1992 <almas1992@126.com>
2025-05-15 14:37:49 +0800

    chore: add readiness check for queue facade in QueueServiceProvider

--- app/providers/queue_service_provider.go
+++ app/providers/queue_service_provider.go
@@ -3,14 +3,21 @@ package providers
 import (
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/contracts/queue"
-	"github.com/goravel/framework/facades"
+	"github.com/goravel/framework/errors"
+	"github.com/goravel/framework/support/color"
 )
 
 type QueueServiceProvider struct {
 }
 
 func (receiver *QueueServiceProvider) Register(app foundation.Application) {
-	facades.Queue().Register(receiver.Jobs())
+	queueFacade := app.MakeQueue()
+	if queueFacade == nil {
+		color.Errorln(errors.QueueFacadeNotSet.SetModule(errors.ModuleQueue))
+		return
+	}
+
+	queueFacade.Register(receiver.Jobs())
 }
 
 func (receiver *QueueServiceProvider) Boot(app foundation.Application) {
